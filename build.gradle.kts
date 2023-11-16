791fbef60ad4ebb7fc74848b182f6346e326887b
funyin <funyin.kash@gmail.com>
2023-11-16 04:52:33 +0100

    maven local publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("multiplatform") version "1.8.21"
     kotlin("plugin.serialization") version "1.9.0"
     id("org.jetbrains.dokka") version "1.9.10"
+    `maven-publish`
 }
 
 group = "com.funyinkash"
